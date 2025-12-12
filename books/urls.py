51e58abafb3b245d4950123f3b7347a45b9f1c32
Amir Karimi <amirmohamadkarimi13901390@gmail.com>
2025-12-12 03:40:37 +0330

    add BookUpdateView

--- books/urls.py
+++ books/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.BookListView.as_view(), name='book_list'),
     path('<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
-    path('create/', views.BookCreateView.as_view(), name='book_create'),
+    path('new/', views.BookCreateView.as_view(), name='book_create'),
+    path('<int:pk>/edit/', views.BookUpdateView.as_view(), name='book_update'),
 ]
