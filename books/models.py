18092e1ffadb70fd8651f7852cd688d3a7c33eaf
Amir Karimi <amirmohamadkarimi13901390@gmail.com>
2025-12-31 14:03:11 -0600

    add Comment model

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class Book(models.Model):
@@ -15,3 +16,13 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.id])
     
+
+class Comment(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    datetime_created = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return self.text
