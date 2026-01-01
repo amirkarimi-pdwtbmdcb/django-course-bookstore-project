dfe1b4572ce788e240fc665ce3c66bb2e3f7cd2a
Amir Karimi <amirmohamadkarimi13901390@gmail.com>
2025-12-31 21:47:41 -0600

    add comment form to book detail view

--- books/views.py
+++ books/views.py
@@ -3,12 +3,14 @@ from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404, render
 
 from .models import Book
+from .forms import CommentForm
+
 
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 6
     template_name = 'books/book_list.html'
-    context_object_name = 'books' 
+    context_object_name = 'books'
 
 
 # class BookDetailView(generic.DetailView):
@@ -21,7 +23,23 @@ def book_detail_view(request, pk):
     # get book comment
     book_comments = book.comments.all()
 
-    return render(request, 'books/book_detail.html', {'book': book, 'comments': book_comments})
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.book = book
+            new_comment.user = request.user
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'books/book_detail.html', {
+        'book': book,
+        'comments': book_comments,
+        'comment_form': comment_form,
+    }
+    )
 
 
 class BookCreateView(generic.CreateView):
@@ -31,7 +49,7 @@ class BookCreateView(generic.CreateView):
 
 
 class BookUpdateView(generic.UpdateView):
-    model = Book 
+    model = Book
     fields = ['title', 'author', 'description', 'price', 'cover', ]
     template_name = 'books/book_update.html'
 
